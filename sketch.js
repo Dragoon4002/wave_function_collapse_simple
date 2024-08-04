5a8d7406c3bfb94d624d99b148a03a2ef41214d8
Dragoon4002 <sounak.dey2468@gmail.com>
2024-08-05 03:07:11 +0530

    Finished Working Wave Function

--- sketch.js
+++ sketch.js
@@ -1,93 +1,70 @@
-let tiles = [];
+const tiles = [];
+const tileImages = [];
 let grid = [];
-let DIM = 50;
+let DIM = 100;
 
-const BLANK = 0;
-const VERTICAL = 1;
-const HORIZONTAL = 2;
-const CROSS = 3;
-const DR = 4;
-const UR = 5;
-const UL = 6;
-const DL = 7;
-
-const rules = [
-    //top,right,down,left
-    [
-        [BLANK, HORIZONTAL, UL, UR],
-        [BLANK, VERTICAL, UR, DR],
-        [BLANK, HORIZONTAL, DR, DL],
-        [BLANK, VERTICAL, UL, DL]
-    ],
-    [
-        [VERTICAL, CROSS, DR, DL],
-        [VERTICAL, BLANK, UR, DR],
-        [VERTICAL, CROSS, UR, UL],
-        [VERTICAL, BLANK, UL, DL]
-    ],
-    [
-        [HORIZONTAL, BLANK, UR, UL],
-        [HORIZONTAL, CROSS, UL, DL],
-        [HORIZONTAL, BLANK, DR, DL],
-        [HORIZONTAL, CROSS, UR, DR]
-    ],
-    [
-        [CROSS, VERTICAL, DR, DL],
-        [CROSS, HORIZONTAL, UL, DL],
-        [CROSS, VERTICAL, UR, UL],
-        [CROSS, HORIZONTAL, UR, DR]
-    ],
-    [
-        [BLANK, HORIZONTAL, UR, UL],
-        [BLANK, VERTICAL, UR, DR],
-        [CROSS, VERTICAL, UR, UL],
-        [CROSS, HORIZONTAL, UL, DL]
-    ],
-    [
-        [CROSS, VERTICAL, DL, DR],
-        [CROSS, HORIZONTAL, UL, DL],
-        [BLANK, HORIZONTAL, DL, DR],
-        [BLANK, VERTICAL, UR, DR]
-    ],
-    [
-        [CROSS, VERTICAL, DL, DR],
-        [BLANK, VERTICAL, UL, DL],
-        [BLANK, HORIZONTAL, DL, DR],
-        [CROSS, HORIZONTAL, DR, UR]
-    ],
-    [
-        [BLANK, HORIZONTAL, UR, UL],
-        [BLANK, VERTICAL, UL, DL],
-        [BLANK, HORIZONTAL, DL, DR],
-        [CROSS, HORIZONTAL, DR, UR]
-    ]
-];
+// const BLANK = 0;
+// const VERTICAL = 1;
+// const HORIZONTAL = 2;
+// const CROSS = 3;
+// const DR = 4;
+// const UR = 5;
+// const UL = 6;
+// const DL = 7;
 
 function preload() {
-    tiles[0] = loadImage("tiles/1.png");
-    tiles[1] = loadImage("tiles/2.png");
-    tiles[2] = loadImage("tiles/3.png");
-    tiles[3] = loadImage("tiles/4.png");
-    tiles[4] = loadImage("tiles/5.png");
-    tiles[5] = loadImage("tiles/6.png");
-    tiles[6] = loadImage("tiles/7.png");
-    tiles[7] = loadImage("tiles/8.png");
+    const path = "tiles";
+    tileImages[0] = loadImage(`${path}/1.png`);
+    tileImages[1] = loadImage(`${path}/2.png`);
+    tileImages[2] = loadImage(`${path}/3.png`);
+    tileImages[3] = loadImage(`${path}/4.png`);
+    tileImages[4] = loadImage(`${path}/5.png`);
+    tileImages[5] = loadImage(`${path}/6.png`);
 }
 
 function setup() {
-    createCanvas(500, 500);
-
-    for (let i = 0; i < DIM * DIM; i++) {
-        grid[i] = {
-            collapsed: false,
-            options: [BLANK, VERTICAL, HORIZONTAL, CROSS, DL, DR, UL, UR],
-        };
+    createCanvas(700, 700);
+    //Loaded and created the tiles
+    tiles[0] = new Tile(tileImages[0], [0, 0, 0, 0]);//Blank
+    tiles[1] = new Tile(tileImages[1], [1, 0, 1, 0]);//Vertical
+    tiles[2] = tiles[1].rotate(1);//Horizontal
+    tiles[3] = new Tile(tileImages[2], [0, 0, 1, 1]);//DL
+    tiles[4] = tiles[3].rotate(1);//UL
+    tiles[5] = tiles[3].rotate(2);//UR
+    tiles[6] = tiles[3].rotate(3);//DR
+    tiles[7] = new Tile(tileImages[3], [1, 1, 1, 1]);//CROSS
+    tiles[8] = new Tile(tileImages[4], [1, 1, 0, 1]);//TUP
+    tiles[9] = tiles[8].rotate(1);//TRIGHT
+    tiles[10] = tiles[8].rotate(2);//TDOWN
+    tiles[11] = tiles[8].rotate(3);//TLEFT
+    tiles[12] = new Tile(tileImages[5], [1, 0, 0, 0]);//END UP
+    tiles[13] = tiles[12].rotate(1);//END RIGHT
+    tiles[14] = tiles[12].rotate(2);//END DOWN
+    tiles[15] = tiles[12].rotate(3);//END LEFT
+    
+    //generating adjucency rule
+    for(let i = 0; i < tiles.length; i++) {
+        const tile = tiles[i];
+        tile.analayze(tiles);
     }
+
+    //cell in everyspot on grid
+    for (let i = 0; i < DIM * DIM; i++) {
+        grid[i] = new Cell(tiles.length)
+    }
+
     noLoop();  // Ensure no looping from the beginning
 }
 
 function mousePressed() {
-    redraw();
+    // Reinitialize the grid
+    grid = [];
+    for (let i = 0; i < DIM * DIM; i++) {
+        grid[i] = new Cell(tiles.length);
+    }
+
+    // Ensure drawing continues until all cells are filled
+    loop();
 }
 
 function checkValid(arr, valid) {
@@ -108,7 +85,7 @@ function draw() {
             let cell = grid[i + j * DIM];
             if (cell.collapsed) {
                 let index = cell.options[0];
-                image(tiles[index], i * w, j * h, w, h);
+                image(tiles[index].img, i * w, j * h, w, h);
             } else {
                 fill(0);
                 stroke(255);
@@ -153,13 +130,13 @@ function draw() {
             if (grid[index].collapsed) {
                 nextGrid[index] = grid[index];
             } else {
-                let options = [BLANK, VERTICAL, HORIZONTAL, UL, UR, DL, DR, CROSS];
-                let validOptions = [];
+                let options = new Array(tiles.length).fill(0).map((x, i)=> i);
                 // Look up
                 if (j > 0) {
                     let up = grid[i + (j - 1) * DIM];
+                    let validOptions = [];
                     for (let option of up.options) {
-                        let valid = rules[option][2];
+                        let valid = tiles[option].down;
                         validOptions = validOptions.concat(valid);
                     }
                     checkValid(options, validOptions);
@@ -167,8 +144,9 @@ function draw() {
                 // Look right
                 if (i < DIM - 1) {
                     let right = grid[i + 1 + j * DIM];
+                    let validOptions = [];
                     for (let option of right.options) {
-                        let valid = rules[option][3];
+                        let valid = tiles[option].left
                         validOptions = validOptions.concat(valid);
                     }
                     checkValid(options, validOptions);
@@ -176,8 +154,9 @@ function draw() {
                 // Look down
                 if (j < DIM - 1) {
                     let down = grid[i + (j + 1) * DIM];
+                    let validOptions = [];
                     for (let option of down.options) {
-                        let valid = rules[option][0];
+                        let valid = tiles[option].up;
                         validOptions = validOptions.concat(valid);
                     }
                     checkValid(options, validOptions);
@@ -185,16 +164,14 @@ function draw() {
                 // Look left
                 if (i > 0) {
                     let left = grid[i - 1 + j * DIM];
+                    let validOptions = [];
                     for (let option of left.options) {
-                        let valid = rules[option][1];
+                        let valid = tiles[option].right;
                         validOptions = validOptions.concat(valid);
                     }
                     checkValid(options, validOptions);
                 }
-                nextGrid[index] = {
-                    options,
-                    collapsed: false,
-                };
+                nextGrid[index] = new Cell(options)
             }
         }
     }
